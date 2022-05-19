0dabdcb7f0693f4178b16ee749c8e75463bb2a51
DanielEngSoft <danielifpi123@gmail.com>
2022-05-19 18:57:25 -0300

    Exercício de Funções 98

---
+++ ExerciciosCursoEmVideo/ex098.py
@@ -0,0 +1,23 @@
+import time
+
+
+def Contador(inicio, fim, passo):
+    print('-='*20)
+    for c in range(inicio, fim, passo):
+        print(c, end=' ')
+        time.sleep(0.3)
+    print('FIM!')
+
+
+Contador(1, 11, 1)
+Contador(10, -1, -2)
+print('Agora é sua vez de personalizar a contagem!')
+i = int(input('INICIO: '))
+f = int(input('FIM: '))
+p = int(input('Passo: '))
+if i > f:
+    p *= -1
+    f -= 1
+else:
+    f += 1
+Contador(i, f, p)

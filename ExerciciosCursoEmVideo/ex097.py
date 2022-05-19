16d01f2911f303fa58c5550fa0856f84b541eed3
DanielEngSoft <danielifpi123@gmail.com>
2022-05-19 08:58:44 -0300

    Exercício de Funções

--- ExerciciosCursoEmVideo/ex097.py
+++ ExerciciosCursoEmVideo/ex097.py
@@ -1,7 +1,8 @@
 def Escreva(frase1):
-    print('~'*len(frase1))
+    print('~' * len(frase1))
     print(frase1)
-    print('~'*len(frase1))
+    print('~' * len(frase1))
+
 
 c = str(input('Frase: '))
 Escreva(c)

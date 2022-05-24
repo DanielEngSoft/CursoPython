ccc33b994ea18e9a879ee7b252863b3c0082a998
DanielEngSoft <danielifpi123@gmail.com>
2022-05-24 07:50:50 -0300

    Exercício de Funções 106 melhorado

--- ExerciciosCursoEmVideo/ex106.py
+++ ExerciciosCursoEmVideo/ex106.py
@@ -1,6 +1,33 @@
-while True:
-    opc = str(input('\033[0;30;41mBiblioteca ou função: '))
-    if opc == 'fim':
-        break
-    help(opc)
+def ajuda(op, c):
+    print(cor[c], end='')
+    help(op)
+    print(cor['p'], end='')
 
+
+def titulo(op, c='p'):
+    print(cor[c], end='')
+    print('='*128)
+    print(f'{op:^127}'.upper())
+    print('='*128)
+    print(cor['p'], end='')
+
+
+# Programa principal
+cor = {
+    'p': '\033[m',
+    'vermelho': '\033[0;30;41m',
+    'verde': '\033[0;30;42m',
+    'amarelo': '\033[0;30;43m',
+    'azul': '\033[0;30;44m',
+    'roxo': '\033[0;30;45m',
+    'branco': '\033[7;48m'
+}
+while True:
+    titulo('SISTEMA DE AJUDA DO DANIEL', 'azul')
+    opc = str(input('DIGITE UMA BIBLIOTECA OU FUNÇÃO: '))
+    if opc.upper().strip() == 'FIM':
+        break
+    else:
+        titulo(opc, 'verde')
+        ajuda(opc, 'amarelo')
+titulo('Até logo!!', 'branco')
